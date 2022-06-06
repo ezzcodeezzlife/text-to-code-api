38d04dd5eab3d0e73f573c4eedf51bf38d758498
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-06 20:15:25 +0200

    initial commit

--- test.js
+++ test.js
@@ -2,12 +2,13 @@ const axios = require("axios");
 
 const options = {
   method: 'POST',
-  url: 'http://localhost:3000/api/text-to-code',
-  data:  {"prompt":"print hello world"},
+  url: 'https://text-to-code-ai.p.rapidapi.com/api/text-to-code',
   headers: {
-    'X-RapidAPI-Host': 'crypto-ai.p.rapidapi.com',
-    'X-RapidAPI-Key': 'fc5f8b88b8mshsdfaf12d278ap105471jsn1e2e43207380'
-  }
+    'content-type': 'application/json',
+    'X-RapidAPI-Host': 'text-to-code-ai.p.rapidapi.com',
+    'X-RapidAPI-Key': 'fc5f8b88b8msh0958fdaf12htfd278ap105471jsn1e2e43207380'
+  },
+  data: '{"prompt":"Add two numbers and return them in javascript"}'
 };
 
 axios.request(options).then(function (response) {
