7fccd9fdfe4fac9581b29763bd82c5ac20ade10a
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-06 20:17:27 +0200

    initial commit

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const options = {
   headers: {
     'content-type': 'application/json',
     'X-RapidAPI-Host': 'text-to-code-ai.p.rapidapi.com',
-    'X-RapidAPI-Key': 'fc5f8b88b8msh0958fdaf12htfd278ap105471jsn1e2e43207380'
+    'X-RapidAPI-Key': 'jksd'
   },
   data: '{"prompt":"Add two numbers and return them in javascript"}'
 };
