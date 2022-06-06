57464236f0224113569537489d8505b3e57f4144
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-06 18:47:15 +0200

    initial commit

--- pages/api/text-to-code.js
+++ pages/api/text-to-code.js
@@ -1,7 +1,59 @@
 import { env } from "process"
+const { Configuration, OpenAIApi } = require("openai")
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 export default async (req, res) => {
 
-  console.log(JSON.parse(req.body).promt)
-  res.send(JSON.parse(req.body).promt)
+  const prompt = JSON.parse(req.body).prompt
+  console.log(prompt)
+  console.log("content length", prompt.length)
+  console.log(req.headers)
+  
+
+  if (prompt.length > 1000) {
+    res.status(400).json({
+      message: "Please under 1000 characters",
+    })
+    return
+  }
+
+  const responseContentFilter = await openai
+    .createCompletion("content-filter-alpha", {
+      prompt: "<|endoftext|>" + prompt + "\n--\nLabel:",
+      temperature: 0,
+      max_tokens: 1,
+      top_p: 0,
+      logprobs: 10,
+  })
+  console.log("content-filter score:", responseContentFilter.data.choices[0].text)
+  
+  if (responseContentFilter.data.choices[0].text !== "0") {
+    res.status(400).json({
+      message: "Your prompt was flagged by the content filter.",
+    })
+    return
+  }
+
+  const response = await openai
+    .createCompletion("text-davinci-002", {
+      prompt:
+        "Code that does the following: " +
+        prompt + 
+        "\n\n\n",
+      temperature: 0.7,
+      max_tokens: 250,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+  })
+  console.log("Response: " + response.data.choices[0].text)
+
+  res.status(200).json({
+      response: response.data.choices[0].text,
+  })
+  
 }
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const options = {
   method: 'GET',
   url: 'http://localhost:3000/api/text-to-code',
-  data:  '{"promt":"asd asd asd asd"}',
+  data:  '{"prompt":"print hello world"}',
   headers: {
     'X-RapidAPI-Host': 'crypto-ai.p.rapidapi.com',
     'X-RapidAPI-Key': 'fc5f8b88b8msh0958fdaf12d278ap105471jsn1e2e43207380'
